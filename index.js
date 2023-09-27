beff62c52c7fe82315cb4470a806ea8cd26a48ba
sondrenils1 <116092604+sondrenils1@users.noreply.github.com>
2023-09-28 01:19:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ let playerScore = 0;
 let startTime = null;
 
 const backgroundMusic = new Audio("heisann.mp3");
-backgroundMusic.loop = true; // Loop the background music // Start playing the background music
+backgroundMusic.loop = true; 
 backgroundMusic.volume = 0.3;
 
   
 const gameOverSound = new Audio("game-over.wav");
-gameOverSound.volume = 0.5 // Replace with the actual sound file
+gameOverSound.volume = 0.5 
 
 
 
@@ -28,27 +28,18 @@ const player = new Player(
 );
 
 const enemies = [
-  //new Enemy(50, 20, "green", 5),
-  //new Enemy(150, 20, "red", 5),
-  //new Enemy(250, 20, "gold", 2),
-  //new Enemy(350, 20, "green", 2),
-  //new Enemy(450, 20, "gold", 10),
-  //new Enemy(50, 100, "green", 5),
-  //new Enemy(150, 100, "red", 5),
-  //new Enemy(250, 100, "gold", 2),
-  //new Enemy(350, 100, "green", 2),
-  //new Enemy(450, 100, "gold", 20),
+ 
 ];
 
 let gameStarted = false;
 let gameOverFlag = false;
 
 function spawnRandomEnemy() {
-  const x = Math.random() * (canvas.width - 50); // Adjust the width of the enemy
-  const y = 0; // Start enemies from the top
+  const x = Math.random() * (canvas.width - 50); 
+  const y = 0; 
   const colors = ["green", "red", "gold"];
-  const color = colors[Math.floor(Math.random() * colors.length)]; // Random color
-  const health = Math.floor(Math.random() * 10) + 1; // Random health between 1 and 10
+  const color = colors[Math.floor(Math.random() * colors.length)]; 
+  const health = Math.floor(Math.random() * 10) + 1; 
   return new Enemy(x, y, color, health);
 }
 
@@ -62,9 +53,9 @@ function startGame() {
   startTime = new Date();
   backgroundMusic.play();
 
-  // Instantiate and push new enemies
+  
   const initialEnemyCount = 2;
-  for (let i = 0; i < initialEnemyCount; i++) { // Adjust the number of initial enemies as needed
+  for (let i = 0; i < initialEnemyCount; i++) { 
     enemies.push(spawnRandomEnemy());
   }
 }
@@ -77,8 +68,6 @@ function gameOver() {
   gameOverSound.play();
 
 
-  // Implement your game over logic here,
-  // such as stopping the game loop, displaying a game over message, etc.
 }
 
 let spawnTimer = 0;
@@ -91,7 +80,7 @@ function gameLoop() {
   player.draw(ctx);
 
   if (!gameStarted) {
-    // Waiting for game start
+    
     ctx.fillStyle = "black";
     ctx.font = "30px Arial";
     ctx.fillText("Press Tab to Start", canvas.width / 2 - 125, canvas.height / 2);
@@ -99,7 +88,7 @@ function gameLoop() {
   }
 
   if (gameOverFlag) {
-    // Display game over message
+    
     ctx.fillStyle = "black";
     ctx.font = "40px Arial";
     ctx.fillText("Game Over", canvas.width / 2 - 100, canvas.height / 2);
@@ -108,7 +97,7 @@ function gameLoop() {
     return;
   }
 
-  // Enemy logic
+
   for (let i = enemies.length - 1; i >= 0; i--) {
     const enemy = enemies[i];
     if (bulletController.collideWith(enemy)) {
@@ -116,14 +105,14 @@ function gameLoop() {
       if (enemy.health <= 0) {
         playerScore += enemy.initialHealth;
         enemies.splice(i, 1);
-        totalDamageDealt += enemy.initialHealth; // Increment total damage dealt
+        totalDamageDealt += enemy.initialHealth; 
         
       }
     } else {
       enemy.draw(ctx);
-      enemy.y += 1; // Move enemy downwards
+      enemy.y += 1; 
       if (enemy.y + enemy.height >= canvas.height) {
-        // Enemy hits the bottom
+       
         gameOver();
         return;
       }
@@ -133,7 +122,7 @@ function gameLoop() {
         player.y < enemy.y + enemy.height &&
         player.y + player.height > enemy.y
       ) {
-        // Enemy collides with player
+        
         gameOver();
         return;
       }
@@ -142,7 +131,7 @@ function gameLoop() {
     ctx.fillText(`Score: ${playerScore}`, canvas.width - 160, 90);
     if (gameStarted) {
       const currentTime = new Date();
-      const elapsedTime = (currentTime - startTime) / 1000; // Convert to seconds
+      const elapsedTime = (currentTime - startTime) / 1000; 
       ctx.fillStyle = "black";
       ctx.font = "20px Arial";
       ctx.fillText(`Time: ${Math.floor(elapsedTime)} seconds`, canvas.width - 160, 30);
@@ -151,27 +140,27 @@ function gameLoop() {
 
 
 
-  // Spawn new enemies based on different intervals
+  
   spawnTimer++;
-  if (spawnTimer % 120 === 0) { // Spawn every 2 seconds
+  if (spawnTimer % 120 === 0) { 
     enemies.push(spawnRandomEnemy());
   }
 
-  // Rest of your game loop logic
+  
 }
 
-// Rest of your code...
+
 
 
 document.addEventListener("keydown", (event) => {
   if (gameOverFlag && event.code === "Enter") {
-    // Restart the game
+    
     startGame();
   } else if (!gameStarted && event.code === "Tab") {
-    // Start the game
+    
     startGame();
   } else {
-    // Handle player input during the game
+    
     player.keydown(event);
   }
 });
